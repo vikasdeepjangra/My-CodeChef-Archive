eb822e2556cf8ef9e798b4ec1e1cb57ff0f055c8
Vikasdeep Jangra <vikasdeepjangra@gmail.com>
2020-07-05 12:58:42 +0530

    5 July 2020

---
+++ PTMSSNG.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+
+ll getOddOccurrence(ll ar[], ll ar_size) 
+{ 
+    ll res = 0;  
+    for (ll i = 0; i < ar_size; i++)      
+        res = res ^ ar[i]; 
+      
+    return res; 
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    
+    while(t--){
+
+        ll n;
+        cin>>n;
+        ll size = ((4*n) - 1);
+
+        ll x[size];
+        ll y[size];
+
+        for(int i=0; i<size; i++){
+            cin>>x[i]>>y[i];
+        }
+
+        ll xc = getOddOccurrence(x,size);
+        ll yc = getOddOccurrence(y,size);
+
+        cout<<xc<<" "<<yc<<endl;
+
+    }
+
+    return 0;
+}
